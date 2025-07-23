fb2d9ccc0ef61817559f90e556f0732a7cbf53fd
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-07-23 11:20:47 +0500

    Add Mapbox map integration to app
    Introduces Mapbox GL JS to the project, adds a map container to the HTML, and initializes a Mapbox map centered at specific coordinates. Updates dependencies to include mapbox-gl.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log( "Friends App" )
+import "mapbox-gl/dist/mapbox-gl.css"
+import mapboxgl from "mapbox-gl"
+
+mapboxgl.accessToken = "pk.eyJ1IjoibmFqaW1vdiIsImEiOiJjbWRmazhzdG0wZHVzMmlzOGdrNHFreWV6In0.ENVcoFkxKIqNeCEax2JoFg"
+
+const map = new mapboxgl.Map( {
+	container: "map",
+	attributionControl: false,
+	logoPosition: "bottom-right",
+	zoom: 9,
+	center: [ 69.2753, 41.3126 ]
+} )
+
+map.on( "load", () => {
+
+	console.info( "Map loaded succesfully!" )
+} )
