ab4e44f36f1b47260c3d0f3335415abd52c977c9
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-07-25 12:17:34 +0500

    Refactor geojson structure and marker logic
    Updated the geojson object to use a simplified structure with a single feature and changed property names for clarity. Marker coordinates are now dynamically set from the geojson feature, and the click handler logs the username instead of a static message. Removed commented-out features for cleaner code.

--- src/main.js
+++ src/main.js
@@ -5,44 +5,18 @@ import mapboxgl from "mapbox-gl"
 mapboxgl.accessToken = "pk.eyJ1IjoibmFqaW1vdiIsImEiOiJjbWRmazhzdG0wZHVzMmlzOGdrNHFreWV6In0.ENVcoFkxKIqNeCEax2JoFg"
 
 const geojson = {
-	'type': 'FeatureCollection',
-	'features': [
+	type: "FeatureCollection",
+	features: [
 		{
-			'type': 'Feature',
-			'properties': {
-				'message': 'Foo',
-				'imageId': 1011,
-				'iconSize': [60, 60]
+			type: "Feature",
+			properties: {
+				username: "Toby",
 			},
-			'geometry': {
-				'type': 'Point',
-				'coordinates': [-66.324462, -16.024695]
+			geometry: {
+				type: "Point",
+				coordinates: [ 69.26121583796288, 41.31552884597011 ]
 			}
 		},
-		// {
-		// 	'type': 'Feature',
-		// 	'properties': {
-		// 		'message': 'Bar',
-		// 		'imageId': 870,
-		// 		'iconSize': [50, 50]
-		// 	},
-		// 	'geometry': {
-		// 		'type': 'Point',
-		// 		'coordinates': [-61.21582, -15.971891]
-		// 	}
-		// },
-		// {
-		// 	'type': 'Feature',
-		// 	'properties': {
-		// 		'message': 'Baz',
-		// 		'imageId': 837,
-		// 		'iconSize': [40, 40]
-		// 	},
-		// 	'geometry': {
-		// 		'type': 'Point',
-		// 		'coordinates': [-63.292236, -18.281518]
-		// 	}
-		// }
 	]
 }
 
@@ -68,11 +42,11 @@ map.on( "load", async () => {
 
 		el.onclick = () => {
 
-			console.log( "CLICKED" )
+			console.log( user.properties.username )
 		}
 
 		const marker = new mapboxgl.Marker( el )
-		marker.setLngLat( [ 69.24848892741892, 41.316625628694 ] )
+		marker.setLngLat( user.geometry.coordinates )
 		marker.addTo( map )
 	}
 } )
