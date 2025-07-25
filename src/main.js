11851578496383623e622c4ad5802ce3587851f8
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-07-25 12:49:55 +0500

    Cleanup

--- src/main.js
+++ src/main.js
@@ -5,34 +5,6 @@ import { io } from "socket.io-client"
 
 mapboxgl.accessToken = "pk.eyJ1IjoibmFqaW1vdiIsImEiOiJjbWRmazhzdG0wZHVzMmlzOGdrNHFreWV6In0.ENVcoFkxKIqNeCEax2JoFg"
 
-const geojson = {
-	type: "FeatureCollection",
-	features: [
-		{
-			type: "Feature",
-			properties: {
-				username: "Toby",
-				avatar: "https://cdn5.vectorstock.com/i/1000x1000/95/04/funny-crocodile-surfing-cartoon-on-white-vector-47309504.jpg",
-			},
-			geometry: {
-				type: "Point",
-				coordinates: [ 69.26121583796288, 41.31552884597011 ]
-			}
-		},
-		{
-			type: "Feature",
-			properties: {
-				username: "Toby",
-				avatar: "https://dbw3zep4prcju.cloudfront.net/animal/6f03d683-43b3-4101-8a62-850ccfab79a3/image/e61d9814-a84c-43f8-a607-1aa962332f95.jpg?versionId=5gqLFUBzVa8uyoeri8DsodBYJqKH12kJ&bust=1752550902&width=600",
-			},
-			geometry: {
-				type: "Point",
-				coordinates: [ 69.26521583796288, 41.31552884597011 ]
-			}
-		},
-	]
-}
-
 const map = new mapboxgl.Map( {
 	container: "map",
 	attributionControl: false,
@@ -60,11 +32,6 @@ map.on( "load", async () => {
 		} )
 
 	} )
-
-	for ( const user of geojson.features ) {
-
-		addNewUser( user, map )
-	}
 } )
 
 function addNewUser( geoJSONFeature, map ) {
